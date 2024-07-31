65708c26db83e839b2176d2d81f18c465683feee
RGB406 <whyrandom456@gmail.com>
2024-07-31 17:58:02 -0400

    Added tesseract to the project (Linked it in the README) and started the picture grabbing of the findString.py

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Bio-Project
 A program that can count the differences in two strings, via image recognition.
+
+Huge credits to the following projects:
+Tesseract - https://github.com/tesseract-ocr/tesseract
+pyTesseract - https://github.com/h/pytesseract
+OpenCV - https://opencv.org/
+Without whom this project would not exist. Thank you!
--- findStrings.py
+++ findStrings.py
@@ -9,13 +9,22 @@ video_feed = ThreadedVideo(name="Webcam Feed")
 video_feed.start_thread()
 # Let the camera boot
 time.sleep(2)
-
+tesseract_path = "tesseract/tesseract.exe"
 
 while True:
+	# Get the image from the webcam to analyze
 	image = video_feed.read()
 	(h, w) = image.shape[:2]
+	pytesseract.pytesseract.tesseract_cmd = tesseract_path
 	
 	# Code for recognizing here.
+	if cv.waitKey(20) % 0xFF == ord("p"):
+		gray_conversion = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+		
+		#_, threshold = cv.threshold(gray_conversion, 0, 255, cv.THRESH_OTSU | cv.THRESH_BINARY_INV)
+
+		cv.imshow("caught", gray_conversion)
+		print(pytesseract.image_to_string(gray_conversion))
 	
 	cv.imshow("frame", image)
 	if cv.waitKey(20) % 0xFF == ord("s"):
